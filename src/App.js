2621776b85d61cf253d2079cea3d5bfb6e4d649b
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-01-19 05:24:55 -0300

    React Router Basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+// Pages
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+
+        <header>
+          <nav>
+            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/about"}>About</NavLink>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+      </BrowserRouter>
   );
 }
 
